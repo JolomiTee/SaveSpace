7a45142508cfa036665c40372f21e66070551dab
Taiwo Jolomi <taiwojolomi15@gmail.com>
2022-09-14 08:31:13 +0100

    modified:   assets/css/style.css 	new file:   assets/js/jquery-3.6.0.min.js 	modified:   assets/js/script.js 	modified:   index.html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// var trunk = document.getElementById('truncate');
+
+// console.log(trunk.innerHTML);
+// if (window.screen.width > 360){
+//     trunk.classList.toggle('truncated');
+
+// } 
+// // else if(window.screen.width < 360){
+// //     trunk.classList.remove('truncated');
+// // }
